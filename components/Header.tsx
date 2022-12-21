4fd1bcbc5f081c45926d26de2db16707563a71ae
Jared Siemen <siemenjm@gmail.com>
2022-12-21 13:54:26 -0500

    Center content on page. Style upper header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
+import { SyntheticEvent } from 'react';
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
+  function handleLogin(e: SyntheticEvent) {
+    e.preventDefault();
+    // TODO: add login function here
+  }
+
   return (
     <header>
-      <div className={styles.loggedInStatusContainer}>Header</div>
+      <div className={styles.loggedInStatusContainer}>
+        <p className={styles.loggedInStatusMessage}>
+          You are currently not logged in
+        </p>
+        <a onClick={handleLogin} className={styles.loginButton}>
+          Log In
+        </a>
+      </div>
     </header>
   );
 }
