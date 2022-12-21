a84e548e055b569d72ddf387414ef72be8926fd8
Jared Siemen <siemenjm@gmail.com>
2022-12-21 14:15:31 -0500

    Style Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,25 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>
+        Practice NextAuth project by{' '}
+        <a
+          href="https://jaredsiemen.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jared Siemen
+        </a>
+        . Followed{' '}
+        <a
+          href="https://next-auth.js.org/getting-started/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          NextAuth documentation
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
