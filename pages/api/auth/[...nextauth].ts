1154a315a0b2b225e1758aa17eeb1908523bb4b7
Jared Siemen <siemenjm@gmail.com>
2022-12-21 14:41:10 -0500

    Add Github provider to auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth/next';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      // @ts-ignore: Ignore possibility that ID is undefined
+      clientId: process.env.GITHUB_ID,
+      // @ts-ignore: Ignore possibility that SECRET is undefined
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
