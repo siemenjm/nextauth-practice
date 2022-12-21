1485ea650c69012c38b976f74c5699cc4488efc1
Jared Siemen <siemenjm@gmail.com>
2022-12-21 13:32:16 -0500

    Add Header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className={styles.loggedInStatusContainer}>Header</div>
+    </header>
+  );
+}
