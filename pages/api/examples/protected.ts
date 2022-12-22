acb12b603cf8f9f27528f1e042612829d085ea19
Jared Siemen <siemenjm@gmail.com>
2022-12-22 12:20:17 -0500

    Add server-side console logs to better see protected api results

--- pages/api/examples/protected.ts
+++ pages/api/examples/protected.ts
@@ -9,6 +9,10 @@ export default async function handler(
   const session = await unstable_getServerSession(req, res, authOptions);
 
   if (session) {
+    console.log(
+      'This is protected content from the /api/examples/protected API route. You can access this content because you are signed in.'
+    );
+
     return res.send({
       content:
         'This is protected content from the /api/examples/protected API route. You can access this content because you are signed in.',
@@ -19,4 +23,8 @@ export default async function handler(
     error:
       'You must be signed in to view the content from the /api/examples/protected API route.',
   });
+
+  console.log(
+    'You must be signed in to view the content from the /api/examples/protected API route.'
+  );
 }
