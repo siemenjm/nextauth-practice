3137df6eadb0179654c1678ab74dd2016cbd14c1
Jared Siemen <siemenjm@gmail.com>
2022-12-22 12:04:31 -0500

    Fetch content from protected API

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,8 +1,23 @@
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 
 export default function ProtectedPage() {
   const { data: session } = useSession();
+  const [content, setContent] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/examples/protected');
+      const json = await res.json();
+      if (json.content) {
+        setContent(json.content);
+      } else if (json.error) {
+        setContent(json.error);
+      }
+    };
+    fetchData();
+  }, [session]);
 
   if (!session) {
     return (
@@ -17,6 +32,7 @@ export default function ProtectedPage() {
     <Layout>
       <h2>Permission Granted</h2>
       <p>You are seeing this page because you are logged in.</p>
+      <p>{content}</p>
     </Layout>
   );
 }
